163a9ff46705646917f022f8c28b28113cfce68f
thiptv <t426lolk@gmail.com>
2026-08-08 18:48:05 -0300

    SEMANA 2 - TEMPLATES

--- hello.py
+++ hello.py
@@ -1,35 +1,30 @@
-# A very simple Flask Hello World app for you to get started with...
-from flask import Flask, request, make_response, redirect, abort
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
 app = Flask(__name__)
+
+# Iniciando extensões
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+# RAÍZ
 @app.route('/')
-def hello_world():
-    return '<p>Alterações por meio do PythonAnyWhere -> GitHub</p><table><tr><td><b>Professor:</b></td><td>Professor Fabio Teixeira</td></tr><tr><td><b>Prontuário:</b></td><td>PT3935999</td></tr></table>'
+def index():
+    return render_template('index.html', current_time = datetime.utcnow())
 
+# IDENTIFICAÇÃO
 @app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+@app.route('/user/<name>/<prontuario>')
+@app.route('/user/<name>/<prontuario>/<instituicao>')
+def user(name, prontuario='PT3036413', instituicao='IFSP'):
+    return render_template('id.html', name=name, prontuario=prontuario, instituicao=instituicao)
 
-@app.route('/contextorequisicao')
-def contextorequisicao():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Você está usando o navegador: {}'.format(user_agent)
-
-@app.route('/codigostatusdiferente')
-def codigostatusdiferente():
-    return '<h1>Bad Request</h1>', 400
-
-@app.route('/objetoresposta')
-def objetoresposta():
-    resposta = make_response('<h1>Document Cookies</h1>')
-    resposta.set_cookie('id_sessao', '12345')
-    return resposta
-
-# REDIRECIONAMENTO
-@app.route('/redirecionamento')
-def redirecionamento():
-    return redirect('https://ptb.ifsp.edu.br/')
-
-# ABORT 404
-@app.route('/abortar')
-def abortar():
-    abort(404)
+# REQUISIÇÃO DE CONTEXTO
+@app.route('/contextorequisicao/<name>')
+def contextorequisicao(name):
+    navegador = request.headers.get('User-Agent')
+    ip_cliente = request.remote_addr
+    host_app = request.host
+    return render_template('contexto.html', name=name, navegador=navegador, ip_cliente=ip_cliente, host_app=host_app)
