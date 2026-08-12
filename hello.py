0c4d64ae3719bb3ee5e32ac24673877e23cf3ef4
th4go <t426lolk@gmail.com>
2026-08-11 21:18:43 -0300

    aula 050

--- hello.py
+++ hello.py
@@ -3,7 +3,16 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'piercetheveil'  # Chave secreta para segurança
 
 # Iniciando extensões
 bootstrap = Bootstrap(app)
@@ -28,3 +37,34 @@ def contextorequisicao(name):
     ip_cliente = request.remote_addr
     host_app = request.host
     return render_template('contexto.html', name=name, navegador=navegador, ip_cliente=ip_cliente, host_app=host_app)
+
+# FORMULÁRIO
+@app.route('/', methods=['GET', 'POST'])
+def index():
+  name = None
+  form = NameForm()
+  if form.validate_on_submit():
+    name = form.name.data
+    form.name.data = ''
+  return render_template('index.html', form=form, name=name)
+
+from flask import Flask, render_template, session, redirect, url_for
+@app.route('/', methods=['GET', 'POST'])
+def index():
+  form = NameForm()
+  if form.validate_on_submit():
+    session['name'] = form.name.data
+    return redirect(url_for('index'))
+  return render_template('index.html', form=form, name=session.get('name'))
+
+from flask import Flask, render_template, session, redirect, url_for, flash
+@app.route('/', methods=['GET', 'POST'])
+def index():
+  form = NameForm()
+  if form.validate_on_submit():
+    old_name = session.get('name')
+    if old_name is not None and old_name != form.name.data:
+      flash('Looks like you have changed your name!')
+    session['name'] = form.name.data
+    return redirect(url_for('index'))
+  return render_template('index.html', form = form, name = session.get('name'))
