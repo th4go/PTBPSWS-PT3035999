545b410f07386ff3251d6dde90150169b47d3fbe
thiptv <t426lolk@gmail.com>
2026-08-23 14:58:14 -0300

    aula do dia 18

--- hello.py
+++ hello.py
@@ -1,49 +1,59 @@
-from flask import Flask, render_template, request
+from datetime import datetime
+from flask import Flask, render_template, session, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
-
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, PasswordField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 
-class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
-    submit = SubmitField('Submit')
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'piercetheveil'  # Chave secreta para segurança
+app.config['SECRET_KEY'] = 'you-will-never-guess'
 
-# Iniciando extensões
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-# IDENTIFICAÇÃO
-@app.route('/user/<name>')
-@app.route('/user/<name>/<prontuario>')
-@app.route('/user/<name>/<prontuario>/<instituicao>')
-def user(name, prontuario='PT3035999', instituicao='IFSP-Pirituba'):
-    return render_template('id.html', name=name, prontuario=prontuario, instituicao=instituicao)
+class NameForm(FlaskForm):
+    name = StringField('Informe o seu nome', validators=[DataRequired()])
+    surname = StringField('Informe o seu sobrenome:', validators=[DataRequired()])
+    institution = StringField('Informe a sua Insituição de ensino:', validators=[DataRequired()])
+    discipline = SelectField('Informe a sua disciplina:', choices=[('DSWA5', 'DSWA5'), ('GPSA5', 'GPSA5'), ('IHCA5', 'IHCA5')])
+    submit = SubmitField('Submit')
 
-# REQUISIÇÃO DE CONTEXTO
-@app.route('/contextorequisicao/<name>')
-def contextorequisicao(name):
-    navegador = request.headers.get('User-Agent')
-    ip_cliente = request.remote_addr
-    host_app = request.host
-    return render_template('contexto.html', name=name, navegador=navegador, ip_cliente=ip_cliente, host_app=host_app)
+class LoginForm(FlaskForm):
+    username = StringField('Usuário ou e-mail', validators=[DataRequired()])
+    password = PasswordField('Informe a sua senha', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
 
-# FORMULÁRIO
-
-from flask import Flask, render_template, session, redirect, url_for, flash
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  form = NameForm()
-  if form.validate_on_submit():
-    old_name = session.get('name')
-    if old_name is not None and old_name != form.name.data:
-      flash('Looks like you have changed your name!')
-    session['name'] = form.name.data
-    return redirect(url_for('index'))
-  return render_template('index.html', form = form, name = session.get('name'))
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        session['surname'] = form.surname.data
+        session['institution'] = form.institution.data
+        session['discipline'] = form.discipline.data
+        return redirect(url_for('index'))
 
+    return render_template(
+        'index.html',
+        form=form,
+        current_time=datetime.utcnow(),
+        remote_ip=request.remote_addr,
+        host=request.host
+    )
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login submetido com sucesso!')
+        return redirect(url_for('index'))
+        
+    return render_template(
+        'login.html',
+        form=form,
+        current_time=datetime.utcnow()
+    )
