466cd641756d78b860df8643814567cb9d84ea1e
thiptv <t426lolk@gmail.com>
2026-08-25 21:41:14 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# Desenvolvimento web com flask
+# Aula 060. Bancos de dados
 
+## Slide 21
+- from hello import db
+- db.create_all()
+
+## Slide 22
+- db.drop_all()
+- db.create_all()
+
+## Slide 23
+- from hello import Role, User
+- admin_role = Role(name='Admin')
+- mod_role = Role(name='Moderator')
+- user_role = Role(name='User')
+- user_john = User(username='john', role=admin_role)
+- user_susan = User(username='susan', role=user_role)
+- user_david = User(username='david', role=user_role)
+
+## Slide 24
+- db.session.add_all([admin_role, mod_role, user_role, user_john, user_susan, user_david])
+
+## Slide 25
+- db.session.commit()
+
+## Slide 26
+- admin_role.name = 'Administrator'
+- db.session.add(admin_role)
+- db.session.commit()
+
+## Slide 27
+- db.session.delete(mod_role)
+- db.session.commit()
+
+## Slide 28
+- Role.query.all()
+- User.query.all()
+- User.query.filter_by(role=user_role).all()
+
+## Slide 30
+- user_role = Role.query.filter_by(name='User').first()
--- hello.py
+++ hello.py
@@ -1,61 +1,80 @@
-from datetime import datetime
-from flask import Flask, render_template, session, redirect, url_for, flash, request
+import os
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SelectField, SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'YOU_WILL_NEVER_GUESS_THIS_SECRET_KEY'
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-@app.context_processor
-def inject_current_time():
-    return dict(current_time=datetime.utcnow())
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 class NameForm(FlaskForm):
-    name = StringField('Informe o seu nome', validators=[DataRequired()])
-    surname = StringField('Informe o seu sobrenome:', validators=[DataRequired()])
-    institution = StringField('Informe a sua Insituição de ensino:', validators=[DataRequired()])
-    discipline = SelectField(
-        'Informe a sua disciplina:',
-        choices=[('DSWA5', 'DSWA5'), ('DWBA4', 'DWBA4'), ('Gestão de Projetos', 'Gestão de Projetos')]
-    )
+    name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-class LoginForm(FlaskForm):
-    username = StringField('Usuário ou e-mail', validators=[DataRequired()])
-    password = PasswordField('Informe a sua senha', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
-        session['surname'] = form.surname.data
-        session['institution'] = form.institution.data
-        session['discipline'] = form.discipline.data
-        
-        session['remote_ip'] = request.headers.get('X-Forwarded-For', request.remote_addr)
-        session['host'] = request.host
         return redirect(url_for('index'))
-
-    return render_template('index.html', form=form)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        session['username'] = form.username.data
-        return redirect(url_for('login_response'))
-        
-    return render_template('login.html', form=form)
-
-@app.route('/login_response')
-def login_response():
-    username = session.get('username')
-    return render_template('login_response.html', username=username)
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False))
